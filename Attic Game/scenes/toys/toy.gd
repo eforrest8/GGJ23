24a755100d5e20da05de07a5cc9e8dced789bb2b
Ethan Forrest <eforrest8@outlook.com>
2023-02-04 11:11:19 -0500

    Improve Toy manipulation
    Rotation and dragging are now separate inputs. Dragging is now constrained to the viewport. Rotating now works smoothly for all angles, rather than 90 degree increments.

--- Attic Game/scenes/toys/toy.gd
+++ Attic Game/scenes/toys/toy.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Sprite
 class_name Toy
 
 # Declare member variables here. Examples:
@@ -13,35 +13,46 @@ func _ready():
 signal grabbed(node)
 signal dropped(node)
 var dragging = false
+var rotating = false
+var grabbed_node = null
 
 
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if $Sprite.get_rect().has_point(to_local(event.position)):
-			# Start dragging if the click is on the sprite.
-			if not dragging and event.pressed:
-				emit_signal("grabbed", self)
-		# Stop dragging if the button is released.
-		if dragging and not event.pressed:
-			emit_signal("dropped", self)
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if get_rect().has_point(to_local(event.position)):
+				# Start dragging if the click is on the sprite.
+				if not dragging and event.pressed:
+					emit_signal("grabbed", self)
+					dragging = true
+			# Stop dragging if the button is released.
+			if dragging and not event.pressed:
+				emit_signal("dropped", self)
+				dragging = false
+		if event.button_index == BUTTON_RIGHT:
+			if get_rect().has_point(to_local(event.position)):
+				if not rotating and event.pressed:
+					emit_signal("grabbed", self)
+					rotating = true
+			if rotating and not event.pressed:
+				emit_signal("dropped", self)
+				rotating = false
 
-	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.is_pressed():
-		if dragging:
-			rotate(PI/2)
+	if event is InputEventMouseMotion and rotating and grabbed_node == self:
+			look_at(get_global_mouse_position())
 
-	if event is InputEventMouseMotion and dragging:
+	if event is InputEventMouseMotion and dragging and grabbed_node == self:
 		# While dragging, move the sprite with the mouse.
-		position = event.position
+		position = Vector2(
+			clamp(event.position.x, 0, get_viewport().size.x),
+			clamp(event.position.y, 0, get_viewport().size.y)
+			)
 
 func _on_Toy_dropped(node):
-	if node == self:
-		dragging = false
+	grabbed_node = null
 
 func _on_Toy_grabbed(node):
-	if node == self:
-		dragging = true
-	else:
-		dragging = false
+	grabbed_node = node
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
