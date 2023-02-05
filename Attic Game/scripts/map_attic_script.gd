c24c7e461b76166800a42894bc9dae931bdabee3
Ethan Forrest <eforrest8@outlook.com>
2023-02-05 15:57:56 -0500

    More stuff

--- Attic Game/scripts/map_attic_script.gd
+++ Attic Game/scripts/map_attic_script.gd
@@ -13,6 +13,10 @@ func _on_bs_floor_interact_body_entered(body):
 	#inventory.visible = false
 	inventory.closeBox()
 	
+func _on_bs_floor_interact_body_exited(body):
+	bookshelf_scene.visible = false
+	#inventory.visible = false
+	inventory.closeBox()
 	
 func _on_exit_button_pressed():
 	bookshelf_scene.visible = false
@@ -29,3 +33,17 @@ func _on_exit_family_scene_pressed():
 
 func _on_grandma_ring_are_body_entered(body):
 	inventory.openBox("cardboard_box")
+
+
+func _on_Button_pressed():
+	var input = $attic_ui_panel/candle_popup/LineEdit.text
+	if input == "436385" or input == "354368" or input == "385643" or input == "386543":
+		pass #win
+
+
+func _on_grandma_ring_are_body_exited(body):
+	inventory.closeBox()
+
+
+func _on_portrait_area_body_exited(body):
+	_on_exit_family_scene_pressed()
