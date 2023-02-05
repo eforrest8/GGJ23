ec98a93ed2e35eaddf0b1a1f9c4f139a355e7910
Ethan Forrest <eforrest8@outlook.com>
2023-02-05 15:34:00 -0500

    Time is running out aaa

--- Attic Game/scripts/map_attic_script.gd
+++ Attic Game/scripts/map_attic_script.gd
@@ -10,7 +10,8 @@ onready var inventory = get_node("inventory")
 # check if you entered area of bookshelf
 func _on_bs_floor_interact_body_entered(body):
 	bookshelf_scene.visible = true
-	inventory.visible = false
+	#inventory.visible = false
+	inventory.closeBox()
 	
 	
 func _on_exit_button_pressed():
@@ -19,10 +20,12 @@ func _on_exit_button_pressed():
 #when walking the area of the portrait the portrait scene appears
 func _on_portrait_area_body_entered(body):
 	family_quiz.visible = true
-	inventory.visible = false
 	
 
 func _on_exit_family_scene_pressed():
 	family_quiz.visible = false
+	inventory.closeBox()
 
-	
+
+func _on_grandma_ring_are_body_entered(body):
+	inventory.openBox("cardboard_box")
